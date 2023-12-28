b7e1a5fd3cd224c03098a67784bf8164ca7c7a73
Alex <alexanderwiegel17@gmail.com>
2023-12-28 15:50:54 +0300

    Add email validator

--- lib/service/validator.dart
+++ lib/service/validator.dart
@@ -1,3 +1,23 @@
+class EmailValidator {
+  static String? validate(String? value) {
+    bool emailValid =
+        RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+                r"{0,253}[a-zA-Z0-9])?)*$")
+            .hasMatch(value!);
+    if (value.isEmpty) {
+      return 'Please enter an email address';
+    } else if (!emailValid) {
+      if (value.contains(' ')) {
+        return null;
+      } else {
+        return 'Please enter a valid email address';
+      }
+    }
+    return null;
+  }
+}
+
 class PasswordValidator {
   static String? validate(String? password) {
     if (password!.isEmpty) {
